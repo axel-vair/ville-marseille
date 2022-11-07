a78640ca835238ce2f9ec438279fa94103527f04
axel-vair <axelvair@icloud.com>
2022-11-07 14:34:35 +0100

    add navbar

---
+++ app.js
@@ -0,0 +1,24 @@
+// Déclaration des variables et pointeur vers les classes
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+// Ajout d'un addEventListener on click qui rajoutera la classe active au toggle. On ouvre la fenêtre.
+hamburger.addEventListener("click" , () => { 
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+// Pour chaque lien, à chaque fois que l'on clique, on enlève la classe active. On ferme la fenêtre.
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>
+{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+
+
+document.addEventListener('scroll', () => 
+{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+})
